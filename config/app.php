8315718cf6a7761b5656c73fb3e362b196a56a0d
christopherlkrt <christopherkrt1@gmail.com>
2018-12-11 19:27:09 -0200

    .

--- config/app.php
+++ config/app.php
@@ -156,9 +156,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        //Collective
-        Collective\Html\HtmlServiceProvider::class,
-
     ],
 
     /*
@@ -204,8 +201,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
